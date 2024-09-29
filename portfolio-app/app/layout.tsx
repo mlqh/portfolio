405adf2225948263630839f58068a15e56458000
mlqh <matt.lqh02@gmail.com>
2024-09-29 03:17:38 -0400

    Add header

--- portfolio-app/app/layout.tsx
+++ portfolio-app/app/layout.tsx
@@ -1,17 +1,6 @@
 import type { Metadata } from 'next';
-import localFont from 'next/font/local';
+import Header from '@/components/header';
 import './globals.css';
-
-const geistSans = localFont({
-  src: './fonts/GeistVF.woff',
-  variable: '--font-geist-sans',
-  weight: '100 900',
-});
-const geistMono = localFont({
-  src: './fonts/GeistMonoVF.woff',
-  variable: '--font-geist-mono',
-  weight: '100 900',
-});
 
 export const metadata: Metadata = {
   title: 'Matthew | Personal Portfolio',
@@ -26,10 +15,12 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-gray-50 text-gray-950`}
+        className={`antialiased bg-gray-50 text-gray-950 relative h-[5000px]`}
       >
         <div className='bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]'></div>
         <div className='bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[15rem] 2xl:left-[-5rem]'></div>
+
+        <Header></Header>
         {children}
       </body>
     </html>
