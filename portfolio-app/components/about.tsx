9df87f119577293c3a1940c5bf791f7cdb7d7528
mlqh <matt.lqh02@gmail.com>
2024-09-30 17:15:07 -0400

    Add header, intro, about section

---
+++ portfolio-app/components/about.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <section className='flex flex-col gap-3 max-w-[45rem] text-center leading-8 sm:mb-40 mb-28'>
+      <h2 className='text-3xl font-medium mb-8'>About Me</h2>
+      <p>
+        I'm currently in my 4th and final year of my{' '}
+        <span className='font-medium'>Computer Engineering</span> degree at the{' '}
+        <span className='font-medium'>University of Waterloo</span>, and I will
+        be graduating in <span className='font-medium'>Spring 2025</span>. I've
+        completed <span className='underline'>5 co-op placements</span> and am
+        currently employed at my 6th, all of which have driven the growth of my
+        passion for programming.
+      </p>
+      <p>
+        During my co-ops, I've worked on all aspects of{' '}
+        <span className='font-medium'>full-stack web development</span>, from
+        API and UI design and development, to testing and deployment, and
+        monitoring and analytics.{' '}
+        <span className='italic'>My favorite part of programming</span> is the
+        problem-solving aspect. I <span className='underline'>love</span> the
+        feeling of finally figuring out the solution to a bug after digging deep
+        into your code for hours, and will stop at nothing to find the optimal
+        solution.
+      </p>
+      <p>
+        My core stack is{' '}
+        <span className='font-medium'>
+          Java, Spring Boot, React.js, and PostgreSQL
+        </span>
+        . I am also familiar with Python, C/C++, Kotlin, and SQL. I am always
+        looking to learn new technologies and stay on top of the ever-growing
+        software development ecosystem. I am currently looking for a{' '}
+        <span className='font-medium'>full-time position</span> as a software
+        developer starting <span className='font-medium'>Summer 2025</span>.
+      </p>
+      <p>
+        <span className='italic'>When I'm not coding</span>, I enjoy playing
+        video games, listening to music, and playing sports. I always try to{' '}
+        <span className='font-medium'>stay curious</span> about the world, and I
+        love all things tech related, whether it be PC's, cars, or mechanical
+        keyboards!
+        <span className='font-medium'></span>
+      </p>
+    </section>
+  );
+}
---
+++ portfolio-app/components/intro.tsx
@@ -0,0 +1,106 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+import { BsArrowRight, BsLinkedin } from 'react-icons/bs';
+import { FaGithubSquare } from 'react-icons/fa';
+import { HiDownload } from 'react-icons/hi';
+
+export default function Intro() {
+  return (
+    <section className='mb-28 max-w-[50rem] text-center sm:mb-0'>
+      <div className='flex items-center justify-center'>
+        <div className='relative'>
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: 'tween',
+              duration: 0.2,
+            }}
+          >
+            <Image
+              src='/cropped-compressed.png'
+              alt='Matthew Portrait'
+              width={192}
+              height={192}
+              quality={80}
+              priority={true}
+              className='h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl'
+            />
+          </motion.div>
+          <motion.span
+            className='text-4xl absolute bottom-0 right-0'
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: 'spring',
+              stiffness: 125,
+              delay: 0.1,
+              duration: 0.7,
+            }}
+          >
+            👋
+          </motion.span>
+        </div>
+      </div>
+
+      <motion.h1
+        className='mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl'
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className='font-bold'>Hi, I&apos;m Matthew.</span> I&apos;m a{' '}
+        <span className='font-bold'>full-stack developer</span> with{' '}
+        <span className='font-bold'>2 years</span> of experience.
+        I enjoy building <span className='italic'>performant systems </span>and
+        the challenges that come with it. My focus is backend development using{' '}
+        <span className='underline'>Java and Springboot</span>.
+      </motion.h1>
+
+      <motion.div
+        className='flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium'
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+      >
+        <Link
+          href='#contact'
+          className='group flex flex-row justify-center bg-gray-900 text-white px-7 py-3 items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition-all'
+        >
+          Contact Me!{' '}
+          <BsArrowRight className='opacity-70 group-hover:translate-x-l transition' />
+        </Link>
+
+        <a
+          className='group flex flex-row bg-white px-7 py-3 items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 border border-black/10 active:scale-105 transition-all cursor-pointer'
+          href='/cv.pdf'
+          download
+        >
+          Download CV{' '}
+          <HiDownload className='opacity-60 group-hover:translate-y-l transition' />
+        </a>
+
+        <a
+          className='bg-white text-gray-700 p-4 items-center gap-2 rounded-full focus:scale-110 hover:scale-110 hover:text-gray-950 border border-black/10 active:scale-105 transition-all cursor-pointer'
+          href='https://linkedin.com/in/mlqh'
+          target='_blank'
+        >
+          <BsLinkedin />
+        </a>
+
+        <a
+          className='bg-white text-gray-700 p-4 items-center gap-2 rounded-full text-[1.35rem] focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 border border-black/10 active:scale-105 transition-all cursor-pointer'
+          href='https://github.com/mlqh'
+          target='_blank'
+        >
+          <FaGithubSquare />
+        </a>
+      </motion.div>
+    </section>
+  );
+}
