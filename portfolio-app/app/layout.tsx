f11d48637c4305b08ae84a9c21bedbd1e96730a6
mlqh <matt.lqh02@gmail.com>
2024-09-30 19:42:37 -0400

    Improve header

--- portfolio-app/app/layout.tsx
+++ portfolio-app/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import Header from '@/components/header';
 import './globals.css';
+import ActiveSectionContextProvider from '@/context/active-section-context';
 
 export const metadata: Metadata = {
   title: 'Matthew | Personal Portfolio',
@@ -19,9 +20,10 @@ export default function RootLayout({
       >
         <div className='bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem]'></div>
         <div className='bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[15rem] 2xl:left-[-5rem]'></div>
-
-        <Header></Header>
-        {children}
+        <ActiveSectionContextProvider>
+          <Header />
+          {children}
+        </ActiveSectionContextProvider>
       </body>
     </html>
   );
--- portfolio-app/components/about.tsx
+++ portfolio-app/components/about.tsx
@@ -3,8 +3,11 @@
 import React from 'react';
 import SectionHeading from './section-heading';
 import { motion } from 'framer-motion';
+import { useSectionInView } from '@/lib/hooks';
 
 export default function About() {
+  const { ref } = useSectionInView('About');
+
   return (
     <motion.section
       id='about'
@@ -12,6 +15,7 @@ export default function About() {
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.175 }}
+      ref={ref}
     >
       <SectionHeading>About me</SectionHeading>
       <p>
